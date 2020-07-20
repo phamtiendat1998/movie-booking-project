e6118c3ebe8a74b355190b29333817e8dea4c3ed
Nguyen Thai Hac <thaihacpk@gmail.com>
2020-07-20 14:23:44 +0700

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+//page
 import { HomePage } from './pages/home/Home.page';
+import { LoginPage } from './pages/login/Login.page';
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      {/* <HomePage></HomePage> */}
+      <LoginPage></LoginPage>
     </div>
   );
 }
